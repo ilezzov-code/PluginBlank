6c2a69e201e54a8c5d7f5ca4b03d954a447a6597
ILeZzoV <ilezov.morozov@yandex.com>
2026-06-19 00:30:43 +0300

    Add PropertiesManager.java and PluginProperties.java

---
+++ src/main/java/ru/ilezzov/pluginBlank/properties/PluginProperties.java
@@ -0,0 +1,8 @@
+package ru.ilezzov.pluginBlank.properties;
+
+public record PluginProperties (
+    String currentVersion,
+    String versionFileUrl,
+    int bstats,
+    String mainCommand
+) {}
---
+++ src/main/java/ru/ilezzov/pluginBlank/properties/PropertiesManager.java
@@ -0,0 +1,34 @@
+package ru.ilezzov.pluginBlank.properties;
+
+import ru.ilezzov.pluginBlank.model.Response;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import static ru.ilezzov.pluginBlank.message.ErrorConstants.IO_ERROR;
+
+public class PropertiesManager {
+    public static Response<PluginProperties> loadProperties(final String fileName) {
+        final Properties properties = new Properties();
+
+        try (final InputStream in = PropertiesManager.class.getClassLoader().getResourceAsStream(fileName)) {
+            properties.load(in);
+
+            return Response.ok(
+                    loadValuesFromProperties(properties)
+            );
+        } catch (final IOException e) {
+            return Response.error(IO_ERROR.formatted(fileName), e);
+        }
+    }
+
+    private static PluginProperties loadValuesFromProperties(final Properties properties) {
+        return new PluginProperties(
+                properties.getProperty("current-version"),
+                properties.getProperty("version-file-url"),
+                Integer.parseInt(properties.getProperty("bstats")),
+                properties.getProperty("main-command")
+        );
+    }
+}
